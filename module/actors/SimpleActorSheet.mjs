b1b8a7ba3b82825fbecda02458d22738b55b0151
Никита Белов <n.belov@sberauto.com>
2023-03-26 20:04:35 +0300

    цели

--- module/actors/SimpleActorSheet.mjs
+++ module/actors/SimpleActorSheet.mjs
@@ -30,10 +30,6 @@ export class CztActorSheet extends BaseActorSheet {
     context.systemData = context.data.system;
     context.config = CONFIG.CZT;
 
-    context.isWeapons = context.items.filter((i) => i.type === "weapon");
-    context.isArmor = context.items.filter((i) => i.type === "armor");
-    context.isEquip = context.items.filter((i) => i.type === "equipment");
-
     game.logger.log(context)
     return context;
   }
@@ -41,11 +37,52 @@ export class CztActorSheet extends BaseActorSheet {
   activateListeners(html) {
     super.activateListeners(html);
 
-    html.find('.sheet-roll-weapon').click(evt => this._onActorRollWeapon(evt));
-    html.find('.sheet-roll-attrs').click(evt => this._onActorRollAttrs(evt));
-
     html.find('.approach-block').click(evt => this._onApprClick(evt));
     html.find('.click-cube-live').click(evt => this._onCubeClick(evt));
+    html.find('.put-goals').click(evt => this._putHeroGoal(evt));
+    html.find('.goal-delete').click(evt => this._delHeroGoal(evt));
+  }
+
+  async _delHeroGoal(evt) {
+    evt.preventDefault();
+    const hero_id = $(evt.currentTarget).closest('table').attr('hero-id');
+    const item_id = $(evt.currentTarget).closest('td').attr('item-id');
+    const heroes = duplicate(this.actor.system.subHeroes);
+    const goals = heroes[hero_id].goals;
+    let els = [];
+    goals.forEach(element => {
+      if(element.id != item_id) {
+        els.push(element);
+      }
+    });
+    heroes[hero_id].goals = els;
+    await this.actor.update({ "system.subHeroes" : heroes } );
+  }
+
+  async _putHeroGoal(evt) {
+    evt.preventDefault();
+    const hero_id = $(evt.currentTarget).closest('div').attr('hero-id');
+    const heroes = duplicate(this.actor.system.subHeroes);
+    if(heroes[hero_id].goals.length == 2) return;
+    let cards = game.items.filter((i) => i.type === "goals");
+    let cards_len = cards.length;
+    if(cards_len === 0) {
+      const pack = game.packs.get(game.system.id + '.goals');
+      cards = await pack.getDocuments();
+      cards_len = cards.length;
+    } 
+    const rnd = CztUtility.getRandomInt(0, cards_len - 1);
+    const item = cards[rnd];
+
+    heroes[hero_id].goals.push({
+      "id": randomID(),
+      "name": item.name,
+      "desc": item.system.description,
+      "item_id": item._id,
+      "img": item.img
+    });
+    
+    await this.actor.update({ "system.subHeroes" : heroes } );
   }
 
   _onCubeClick(evt) {
@@ -69,89 +106,6 @@ export class CztActorSheet extends BaseActorSheet {
     this.actor.update({ "system.approaches" : approaches } );
   }
  
-  async _onActorRollWeapon(evt) {
-    evt.preventDefault();
-    const weapon_id = $(evt.currentTarget).closest('tr').attr('item-id');
-    const item = this.actor.items.filter((i) => i.type === "weapon" && i.id == weapon_id);
-    const oItem = game.items.get(item[0].item_id);
-    
-    let roll = await new Roll(item[0].formula).roll({async: true});
-
-    const html = await renderTemplate(`${game.system_path}/templates/chats/weapon-roll.hbs`, {
-      item_name: item[0].name,
-      img: item[0].img,
-      dice: item[0].formula,
-      desc: oItem.system.description,
-      result: roll.result,
-      total: roll.total
-    });
-
-    ChatMessage.create({
-      user: game.user._id,
-      speaker: ChatMessage.getSpeaker(),
-      content: html
-    });
-  }
-
-  async checkAttr(attr, html) {
-    // Оставлю себе для примера
-    const actor_min = this.actor.system.attrs[attr].curr;
-    const actor_max = this.actor.system.attrs[attr].max;
-
-    const dices = html.find(`form input[name=count_dices]`).val();
-    const mod = html.find(`form input[type=radio][name=modificator]:checked`).val();
-
-    let roll = await new Roll(`${dices}d20`).evaluate({async: true});
-    let sortedResults = roll.terms[0].results.map(r => {return r.result}).sort(function(a, b) {
-      return b - a;});
-    
-      const tpl = await renderTemplate(`${game.system_path}/templates/chats/attrs-roll.hbs`, {
-        terms: `${dices}d20`,
-        row: sortedResults.join(', '),
-        rmax: parseInt(sortedResults[0]),
-        rmin: parseInt(sortedResults.slice(-1)),
-        attr: attr,
-        mod: mod,
-        actor_min: parseInt(actor_min),
-        actor_max: parseInt(actor_max),
-        attrLabel: CONFIG.CZT.Attrs[attr]
-      });
-  
-    ChatMessage.create({
-        user: game.user._id,
-        speaker: ChatMessage.getSpeaker(),
-        content: tpl
-    });
-  }
-
-  async _onActorRollAttrs(evt) {
-    evt.preventDefault();
-    return false; // Заглушка
-    const attrType = $(evt.currentTarget).attr('attr-type'); 
-
-    const template = await renderTemplate(`${game.system_path}/templates/dialogs/attrs-roll.hbs`);
-    return new Promise(resolve => {
-      const data = {
-        title: "", // game.i18n.localize("CZT.Common.CheckAttrs"),
-        content: template,
-        buttons: {
-          cancel: {
-            icon: '<i class="fas fa-times"></i>',
-            label: game.i18n.localize("CZT.Common.Buttons.Cancel"),
-            callback: html => resolve({cancelled: true})
-          },
-          yes: {
-            icon: '<i class="fas fa-check"></i>',
-            label: game.i18n.localize("CZT.Common.Select.Yes"),
-            callback: html => resolve(this.checkAttr(attrType, html))
-           }        
-        },
-        default: "cancel",
-        close: () => resolve({cancelled: true})
-      }
-      new Dialog(data, null).render(true);
-    });
-  }
   /** @override */
   async _onDrop(evt) { 
     evt.preventDefault();
